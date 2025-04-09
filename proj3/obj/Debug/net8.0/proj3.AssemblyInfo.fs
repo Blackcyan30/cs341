a476712ceede0a41364971fda86a25c0312b9b59
talha <talhaadnan2003@gmail.com>
2025-04-09 18:21:50 -0500

    Dunno why I need to do this since there is nothing that I updated in code

--- proj3/obj/Debug/net8.0/proj3.AssemblyInfo.fs
+++ proj3/obj/Debug/net8.0/proj3.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("proj3")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3797bcde0504048c52e39ef429b164a17fac925a")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73424de42f59929f7bce418ccf7251dd00c61fdb")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("proj3")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("proj3")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
