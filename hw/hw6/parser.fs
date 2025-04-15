9f151de12cbbca9b3b9a4f21c030eb63a2279151
talha <talhaadnan2003@gmail.com>
2025-04-14 22:15:09 -0500

    got the intial working down

--- hw/hw6/parser.fs
+++ hw/hw6/parser.fs
@@ -9,6 +9,10 @@ module parser =
   // NOTE: all functions in the module must be indented.
   //
 
+  let beginsWith (pattern: string) (literal: string) =
+    literal.StartsWith (pattern)
+
+
   //
   // matchToken
   //
@@ -27,6 +31,53 @@ module parser =
     else
       failwith ("expecting " + expected_token + ", but found " + next_token)
 
+  let empty tokens = 
+    matchToken ";" tokens
+
+  let stmt tokens = 
+    printfn "stmt %A" tokens
+    match tokens with
+    | ";" :: _ -> empty tokens 
+    | "cout" :: _ -> [] // output tokens
+    | _ -> tokens
+
+  let rec moreStmts tokens = 
+    match tokens with
+    | "}" :: _ -> tokens 
+    | _ -> stmt tokens |> moreStmts
+
+
+
+  let rec stmts tokens = 
+    // match token with
+    // | [] -> []
+    // | x -> 
+    // | statement :: moreStatements ->
+    //   matchToken ""
+    //   moreStmts moreStatements
+    printfn "stmts: %A" tokens
+    tokens
+    |> stmt
+    |> moreStmts
+
+  let exprValue tokens =
+    match tokens with
+    | "true" -> matchToken "true" tokens
+    | "false" -> matchToken "false" tokens
+    | _ -> if beginsWith "str_literal" tokens then
+             List.tail tokens
+           else if beginsWith "int_literal" tokens then
+             List.tail tokens
+           else
+            failwith("expecting literal, but found" + token)
+
+
+
+
+    // let t2 = stmt tokens
+    // let t3 = moreStmts t2
+    // t3
+  
   //
   // verySimpleC
   //
@@ -35,7 +86,17 @@ module parser =
     // TODO: Start here by filling it in and
     //       creating your other functions!
     //
-    matchToken "$" tokens
+    let t2 = matchToken "void" tokens
+    let t3 = matchToken "main" t2
+    let t4 = matchToken "(" t3
+    let t5 = matchToken ")" t4
+    let t6 = matchToken "{" t5
+    let t7 = stmts t6
+    let t8 = matchToken "}" t7
+    let t9 = matchToken "$" t8
+    t9
+
+    // matchToken "$" tokens
 
   //
   // parse tokens
